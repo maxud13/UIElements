0dc4695f3980b1995447dae45db250cce16a89d0
maxud <mlenon13@gmail.com>
2017-09-03 16:51:30 +0300

    лекция

---
+++ UIElements/UIElements/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIElements
+//
+//  Created by maxud on 01.09.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIElements/UIElements/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIElements
+//
+//  Created by maxud on 01.09.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
